b29302f1a1fe693d53d2ed73b410c0f6873b1512
Lessie Quezada <Lessieq07@gmail.com>
2021-06-12 14:29:27 -0400

    Imported the Searchgif file to see if it is working correctly

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Searchgif from './Searchgif';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Searchgif/>
   </React.StrictMode>,
   document.getElementById('root')
 );
