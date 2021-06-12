8500620577529a905d337e477715736fbdd1ad43
Lessie Quezada <Lessieq07@gmail.com>
2021-06-12 14:27:52 -0400

    Added a Search.gif JS file and added a class

---
+++ src/Searchgif.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Searchgif extends React.Component{
+    constructor(props){
+
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <h1>Hello World
+                
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Searchgif;
